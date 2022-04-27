29333fabbfae671522059bd31a2c9a501598c329
Drew Stackhouse <drewstckhs@gmail.com>
2022-04-26 22:13:13 -0400

    changed color scheme

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,18 @@
 <template>
   <v-app>
     <v-main>
-      <v-app-bar color="blue-grey darken-4" v-if="$route.path !== '/'" flat app height="80">
+      <!--
+        old scheme:
+          base: blue-grey darken-4
+          accent: amber darken-4
+      -->
+      <v-app-bar color="grey darken-4" v-if="$route.path !== '/'" flat app height="80">
       <v-container fluid class="text-center">
         <v-row align="center">
           <v-col>
 
         <!--<span :class="mobile ? 'text-h4 white--text font-weight-light' : 'text-h3 white--text font-weight-light'"><v-icon :style="mobile ? 'font-size: 40px' : 'font-size: 50px'" color="amber darken-4" class="pr-3 mb-2">mdi-translate</v-icon>Neural Translate</span>-->
-        <span :class="mobile ? 'text-h5 white--text font-weight-light' : 'text-h4 white--text font-weight-light'"><v-icon x-large color="amber darken-4" class="pr-3 mb-2">mdi-translate</v-icon>Neural Translate</span>          </v-col>
+        <span :class="mobile ? 'text-h5 white--text font-weight-light' : 'text-h4 white--text font-weight-light'"><v-icon x-large color="light-green" class="pr-3 mb-2">mdi-translate</v-icon>Neural Translate</span>          </v-col>
           <!--<v-col class="text-right">
             <v-btn text x-large color="amber darken-4">About</v-btn>
             <v-btn text x-large color="amber darken-4">Contribute</v-btn>
@@ -18,16 +23,16 @@
       <v-fade-transition mode="out-in">
         <Nuxt />
       </v-fade-transition>
-      <v-footer app color="blue-grey darken-4" v-if="$route.path !== '/' && !mobile">
+      <v-footer app color="grey darken-4" v-if="$route.path !== '/' && !mobile">
       <v-container fluid fill-height>
         <v-row align="center" justify="center">
         <!--  <v-col class="">
         <span class="white--text font-weight-light text-body-1">Made by <a href="https://www.drewstackhouse.com" rel="noopener noreferrer" target="_blank" style="color: rgba(255, 111, 0, 1)">Drew Stackhouse</a></span>
           </v-col>-->
           <v-col class="text-center">
-        <v-btn icon><v-icon color="amber darken-4">mdi-twitter</v-icon></v-btn>
-        <v-btn icon><v-icon color="amber darken-4">mdi-linkedin</v-icon></v-btn>
-        <v-btn icon><v-icon color="amber darken-4">mdi-github</v-icon></v-btn>
+        <v-btn icon><v-icon color="light-green">mdi-twitter</v-icon></v-btn>
+        <v-btn icon><v-icon color="light-green">mdi-linkedin</v-icon></v-btn>
+        <v-btn icon><v-icon color="light-green">mdi-github</v-icon></v-btn>
           </v-col>
         </v-row>
       </v-container>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-container fluid fill-height v-if="showIntro">
     <v-row align="center" justify="center">
       <v-col class="text-center" cols="12">
-        <span :class="mobile ? 'text-h5 font-weight-regular' : 'text-h3 font-weight-light'"><v-icon :style="mobile ? 'font-size: 30px' : 'font-size: 50px'" color="amber darken-4" :class="mobile ? 'pr-3 mb-2' : 'pr-6 mb-2'">mdi-translate</v-icon>Neural Translate</span>
+        <span :class="mobile ? 'text-h5 font-weight-regular' : 'text-h3 font-weight-light'"><v-icon :style="mobile ? 'font-size: 30px' : 'font-size: 50px'" color="light-green" :class="mobile ? 'pr-3 mb-2' : 'pr-6 mb-2'">mdi-translate</v-icon>Neural Translate</span>
       </v-col>
       <v-col class="text-center">
         <v-carousel
@@ -29,7 +29,7 @@
         <v-progress-circular :size="mobile ? 100 : 200"
           width="5"
           indeterminate
-          color="amber darken-4"
+          color="light-green"
         ></v-progress-circular>
       </v-col>
     </v-row>
